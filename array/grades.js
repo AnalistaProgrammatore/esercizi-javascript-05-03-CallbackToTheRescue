3205364032b6142215de0697124a1484abcf3c37
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 02:41:02 +0100

    Update grades.js

--- array/grades.js
+++ array/grades.js
@@ -1,1 +1,25 @@
+function Grades(name) {
+    this._name = name
+    this.getStudentGrades = function () {
+        return `The student ${this._name} has the following grades: ${this._grade}`
+    }
+    this.setStudentGrades = function (...value) {
+        const [...grade] = value
+        this._grade = grade
+    }
+    this.average = function () {
+        this.getAverage = null
+        this.numberOfGrades = this._grade.length
+        this.sum = this._grade.reduce(
+            function (acc, val) {
+                return (acc + val)
+            })
+        this.getAverage = this.sum / this.numberOfGrades
+        return ` The grade's average of ${this._name} is ${this.getAverage}`
+    }
+}
 
+const studente1 = new Grades('Edoardo')
+studente1.setStudentGrades(5, 4, 7, 8, 6, 5)
+console.log(studente1.getStudentGrades())
+console.log(studente1.average())
