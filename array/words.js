f7c111292334be04c32a9ed1575728a2d7922b56
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 02:54:48 +0100

    Update words.js

--- array/words.js
+++ array/words.js
@@ -1,1 +1,5 @@
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+let reverse = days.reduceRight((curr, val) => (curr.push(val), curr), []);
 
+console.log(days)
+console.log(reverse)
