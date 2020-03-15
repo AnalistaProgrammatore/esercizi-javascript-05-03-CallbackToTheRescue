456668b5a7869db51fccf58d8cf248374f601938
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 03:20:08 +0100

    Update calculator.js

--- objects/calculator.js
+++ objects/calculator.js
@@ -1,1 +1,27 @@
+let calculator = {
+    set values(values) {
+        this.a = values[0]
+        this.b = values[1]
+    },
+    add() {
+        return this.a + this.b
+    },
+    sub() {
+        return this.a - this.b
+    },
+    mul() {
+        return this.a * this.b
+    },
+    div() {
+        if (this.b === 0) {
+            const arr = [this.add(), this.sub(), this.mul()]
+            return arr.reduce((acc, val) => [...acc, val], [])
+        } else { return this.a / this.b }
+    }
+}
 
+calculator.values = [10, 0]
+console.log(calculator.add())
+console.log(calculator.sub())
+console.log(calculator.mul())
+console.log(calculator.div())
