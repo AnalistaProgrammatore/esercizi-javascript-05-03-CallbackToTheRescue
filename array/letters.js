99cd07ebca8a379422201ceda9666da75b4ddd43
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 03:10:04 +0100

    Update letters.js

--- array/letters.js
+++ array/letters.js
@@ -1,1 +1,9 @@
+const Letter = function (...word) {
+    this.word = word
+    this.showLetter = function () {
+        return word.reduce((acc, val) => acc + val)
+    }
+}
 
+const word = new Letter('h', 'e', 'l', 'l', 'o', '!')
+console.log(word.showLetter())
