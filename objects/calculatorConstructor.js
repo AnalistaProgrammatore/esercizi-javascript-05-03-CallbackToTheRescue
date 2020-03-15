1543afe11ce846849006dfbf3799e69b45f96bbd
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 03:22:17 +0100

    Update calculatorConstructor.js

--- objects/calculatorConstructor.js
+++ objects/calculatorConstructor.js
@@ -1,1 +1,14 @@
+const Calculator = function (value = []) {
+    this.x = value[0]
+    this.y = value[1]
+    this.sum = function () { return this.x + this.y }
+    this.sub = function () { return this.x - this.y }
+    this.mul = function () { return this.x * this.y }
+    this.div = function () { return this.x / this.y }
+}
 
+const calculator = new Calculator([10, 5])
+console.log(calculator.sum())
+console.log(calculator.sub())
+console.log(calculator.mul())
+console.log(calculator.div())
