4f8fcd3ddcb17ebec789577a68a9d662c13048ab
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 03:14:46 +0100

    Update multiply.js

--- objects/multiply.js
+++ objects/multiply.js
@@ -1,1 +1,17 @@
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+}
 
+const multiplyNumeric = function (obj, num) {
+    const newObj = {}
+    newObj.width = obj.width * num
+    newObj.height = obj.height * num
+    newObj.title = obj.title
+    return newObj
+}
+
+const newMenu = multiplyNumeric(menu, 2)
+console.log('old menu', menu)
+console.log('new menu', newMenu)
