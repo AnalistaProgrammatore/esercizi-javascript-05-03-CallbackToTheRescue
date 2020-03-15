836617bd7284b7f2c7946886222f2713b36e67cf
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 03:21:15 +0100

    Update chaining.js

--- objects/chaining.js
+++ objects/chaining.js
@@ -1,1 +1,18 @@
-
+class Ladder {
+    constructor() {
+        this.step = 0
+    }
+    up() {
+        this.step++
+        return this
+    }
+    down() {
+        this.step--
+        return this
+    }
+    showStep() {
+        console.log(this.step)
+    }
+}
+const myLadder = new Ladder()
+myLadder.up().up().down().showStep()
