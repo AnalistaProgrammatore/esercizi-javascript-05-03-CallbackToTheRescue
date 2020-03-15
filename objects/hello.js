1a09e2ce356a077c34118f193558c3c81a03ab74
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 03:12:17 +0100

    Update hello.js

--- objects/hello.js
+++ objects/hello.js
@@ -1,1 +1,16 @@
+const user = {}
 
+user.name = 'Mario'
+user.surname = 'Rossi'
+
+Object.defineProperties(user, {
+  'surname': {
+    configurable: false,
+    writable: false
+  }
+})
+
+user.name = 'Marco'
+delete user.name
+
+console.log(user)
