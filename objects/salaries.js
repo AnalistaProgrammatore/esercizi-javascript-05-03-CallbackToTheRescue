45c57e2d6c1c8260353427e9a82a7086b86ba8e4
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-15 03:13:40 +0100

    Update salaries.js

--- objects/salaries.js
+++ objects/salaries.js
@@ -1,1 +1,16 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
 
+const totalSalaries = function (obj) {
+    let total = 0
+    for (let i = 0; i < Object.keys(obj).length; i++) {
+        total = Object.values(obj).reduce((acc, val) => acc + val)
+    }
+    return total
+}
+
+const prova = totalSalaries(salaries)
+console.log(prova)
