c98d143858580e1f37f1e810b0db5e2fafc8e28e
CallbackToTheRescue <59921779+CallbackToTheRescue@users.noreply.github.com>
2020-03-16 02:24:50 +0100

    Update calculatorConstructor.js

--- objects/calculatorConstructor.js
+++ objects/calculatorConstructor.js
@@ -1,14 +1,24 @@
-const Calculator = function (value = []) {
-    this.x = value[0]
-    this.y = value[1]
-    this.sum = function () { return this.x + this.y }
-    this.sub = function () { return this.x - this.y }
-    this.mul = function () { return this.x * this.y }
-    this.div = function () { return this.x / this.y }
+// ovviamente l'errore che avevo commesso si ripercuoteva anche qui, questa è la versione corretta
+const Calculator = function (values) {
+    this.values = values
+    console.log('this values contiene', this.values)
+    this.add = function () {
+        return this.values.reduce((acc, val) => acc + val)
+    },
+        this.sub = function () {
+            return this.values.reduce((acc, val) => acc - val)
+        },
+        this.mul = function () {
+            return this.values.reduce((acc, val) => acc * val)
+        },
+        this.div = function () {
+            const [...a] = this.values
+            return (a.includes(0)) ? 'You cannot divide by zero' : this.values.reduce((acc, val) => acc / val)
+        }
 }
 
-const calculator = new Calculator([10, 5])
-console.log(calculator.sum())
+const calculator = new Calculator([10, 5, 0])
+console.log(calculator.add())
 console.log(calculator.sub())
 console.log(calculator.mul())
 console.log(calculator.div())
